ab38bd317f00d81498551999eb454825a7b75340
Raphael Scursoni <ra.scursoni@gmail.com>
2022-03-14 11:09:33 -0300

    Exercitando Herança C#

---
+++ ExercicioPolimorfismo/Entities/ImportedProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioPolimorfismo.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public ImportedProduct(double customsFee, string name, double price)
+            : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
---
+++ ExercicioPolimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioPolimorfismo.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(DateTime manufactureDate, string name, double price)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
